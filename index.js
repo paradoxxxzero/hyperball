59a89a41d2cda88aaa0555b51c46affd632e5682
Florian Mounier <florian.mounier@kozea.fr>
2021-07-05 15:51:24 +0200

    Add spherical projection

--- index.js
+++ index.js
@@ -85,7 +85,17 @@ let radius,
   queueTimeout,
   reverting,
   backend = '2d',
-  stop = false
+  stop = false,
+  model
+
+const getModel = ({ p, q, r }) => {
+  const anglesSum = 1 / p + 1 / q + 1 / r
+  return anglesSum === 1
+    ? 'parabolic'
+    : anglesSum < 1
+    ? 'hyperbolic'
+    : 'elliptic'
+}
 
 const getPreset = () =>
   decodeURIComponent(location.hash.replace(/^#/, '')) || presets.preset
@@ -94,6 +104,7 @@ const settings = {
   ...(presets.remembered[preset] || presets.remembered[presets.preset])[0],
 }
 tokenSize = 10 ** settings.tokenPrecision
+model = getModel(settings)
 const FILL_COLOR_TYPES = ['none', 'plain', 'odd', 'colored']
 const STROKE_COLOR_TYPES = ['none', 'plain', 'colored']
 
@@ -110,17 +121,18 @@ const size = force => {
 
   canvas.width = width
   canvas.height = height
-  // }
+
   w2 = width / 2
   h2 = height / 2
   const s2 = Math.min(w2, h2)
   radius = {
-    inverted: 0.4 * s2,
     poincare: 0.9 * s2,
     klein: 0.9 * s2,
+    inverted: 0.4 * s2,
     band: 0.102 * h2,
     half: 0.102 * h2,
-    ortho: 0.3 * s2,
+    stereographic: 1 * s2,
+    orthographic: 0.3 * s2,
     joukowsky: 0.8 * w2,
   }[settings.projection]
 
@@ -135,32 +147,32 @@ const showStats = { showStats: false, '3dControls': false }
 
 const toDisk = ([x, y]) => [w2 + x * radius, h2 - y * radius]
 
-const poincareFromHyperboloid = ([x, y, z]) => {
+const poincare = ([x, y, z]) => {
   // z = 1
   const nr = 1 / (1 + z)
   return [x * nr, y * nr]
 }
 
-const kleinFromHyperboloid = ([x, y, z]) => {
+const klein = ([x, y, z]) => {
   // z = 0
   const nr = 1 / z
   return [x * nr, y * nr]
 }
 
-const orthoFromHyperboloid = ([x, y, z]) => {
+const orthographic = ([x, y, z]) => {
   // No z
   return [x, y]
 }
 
-const invertedFromHyperboloid = ([x, y, z]) => {
+const inverted = ([x, y, z]) => {
   // z = -1
   const nr = 1 / (-1 + z)
   return [x * nr, y * nr]
 }
 
-const bandFromHyperboloid = ([x, y, z]) => {
+const band = ([x, y, z]) => {
   // 4 * Math.PI * arctanh(z)
-  const [X, Y] = poincareFromHyperboloid([x, y, z])
+  const [X, Y] = poincare([x, y, z])
   const k = 4 * Math.PI * 0.5
   const d1 = (1 + X) * (1 + X) + Y * Y
   const d2 = (1 - X) * (1 - X) + Y * Y
@@ -170,62 +182,72 @@ const bandFromHyperboloid = ([x, y, z]) => {
   ]
 }
 
-const halfFromHyperboloid = ([x, y, z]) => {
+const half = ([x, y, z]) => {
   // 4 * Math.PI * arctanh(z)
-  const [X, Y] = poincareFromHyperboloid([x, y, z])
+  const [X, Y] = poincare([x, y, z])
   const X2 = X * X
   const Y1 = Y - 1
   const k = -1 / (X2 + Y1 * Y1)
   return [2 * X * k, (X2 + Y1 * (Y + 1)) * k]
 }
 
-const joukowskyFromHyperboloid = ([x, y, z]) => {
-  const [X, Y] = poincareFromHyperboloid([x, y, z])
+const joukowsky = ([x, y, z]) => {
+  const [X, Y] = poincare([x, y, z])
   const nr = 1 / (2 * (X * X + Y * Y))
 
   return [X * (0.5 + nr), Y * (0.5 - nr)]
 }
 
+const stereographic = ([x, y, z]) => {
+  // z = 0
+  const nr = 1 / (1 - z)
+  return [x * nr, y * nr]
+}
+
 const projections = {
-  poincare: poincareFromHyperboloid,
-  klein: kleinFromHyperboloid,
-  inverted: invertedFromHyperboloid,
-  band: bandFromHyperboloid,
-  half: halfFromHyperboloid,
-  ortho: orthoFromHyperboloid,
-  joukowsky: joukowskyFromHyperboloid,
+  poincare: poincare,
+  klein: klein,
+  inverted: inverted,
+  band: band,
+  half: half,
+  orthographic: orthographic,
+  joukowsky: joukowsky,
+  stereographic: stereographic,
 }
 
 const views = ['3d poincare', '3d klein', '3d inverted']
 
-const fromHyperboloid = p => projections[settings.projection](p)
+const project = p => projections[settings.projection](p)
 
 const normalize = ([x, y, z]) => {
-  const s = z < 0 ? -1 : 1
-  const k = s / Math.sqrt(-x * x - y * y + z * z)
-  return [x * k, y * k, z * k]
+  if (model === 'hyperbolic') {
+    const k = Math.sign(z) / Math.sqrt(-x * x - y * y + z * z)
+    return [x * k, y * k, z * k]
+  } else {
+    const k = 1 / Math.sqrt(x * x + y * y + z * z)
+    return [x * k, y * k, z * k]
+  }
 }
 
-const intersect = ([xa, ya, za], [xb, yb, zb]) => {
-  const cross = [
-    //
-    ya * zb - za * yb,
-    za * xb - xa * zb,
-    ya * xb - xa * yb, // hyper cross : -z
-  ]
-  return normalize(cross)
+const sign = () => (model === 'hyperbolic' ? -1 : model === 'parabolic' ? 0 : 1)
+
+const dot = ([xa, ya, za], [xb, yb, zb]) => xa * xb + ya * yb + sign() * za * zb
+
+const cross = ([xa, ya, za], [xb, yb, zb]) => [
+  //
+  ya * zb - za * yb,
+  za * xb - xa * zb,
+  sign() * (xa * yb - ya * xb),
+]
+
+const bisect = (a, b) => intersect(cross(a, b), 0.5 * (a + b))
+
+const reflect = (a, b) => {
+  const ab2 = (2 * dot(a, b)) / dot(b, b)
+  return [a[0] - ab2 * b[0], a[1] - ab2 * b[1], a[2] - ab2 * b[2]]
 }
 
-const reflect = (point, reflector) => {
-  const [x, y, z] = point
-  const [a, b, c] = reflector
-
-  const xyzabc = x * a + y * b - z * c
-  const abcabc = a * a + b * b - c * c
-  const k = (2 * xyzabc) / abcabc
-
-  return [x - k * a, y - k * b, z - k * c]
-}
+const intersect = (a, b) => normalize(cross(a, b))
 
 const getRootTriangle = () => {
   const pAngle = Math.PI / settings.p
@@ -235,12 +257,12 @@ const getRootTriangle = () => {
   const a =
     (Math.cos(pAngle) * Math.cos(qAngle) + Math.cos(rAngle)) / Math.sin(pAngle)
   const b = Math.cos(qAngle)
-  const c = Math.sqrt(a * a + b * b - 1)
+  const c = Math.sqrt(sign() * (1 - a * a - b * b))
 
   return [
-    [-b, a, c],
-    [-Math.cos(pAngle), -Math.sin(pAngle), 0],
     [1, 0, 0],
+    [-Math.cos(pAngle), Math.sin(pAngle), 0],
+    [b, a, c],
   ]
 }
 
@@ -267,12 +289,29 @@ const getToken = edges => {
   return str.sort().join('/')
 }
 
+// const reflectOn = (edges, i, order) => {
+//   let j = (i + 1) % 3
+//   let k = (i + 2) % 3
+//   edges[j] = reflect(edges[j], edges[i])
+//   edges[k] = reflect(edges[k], edges[i])
+//   edges[i] = [-edges[i][0], -edges[i][1], -edges[i][2]]
+
+//   const token = getToken(edges)
+//   for (let i = order; i >= 0; i--) {
+//     if (tokens[i][token]) {
+//       return false
+//     }
+//   }
+//   tokens[order][token] = true
+//   return true
+// }
+
 const reflectOn = (edges, i, order) => {
   let j = (i + 1) % 3
   let k = (i + 2) % 3
   edges[j] = reflect(edges[j], edges[i])
   edges[k] = reflect(edges[k], edges[i])
-  edges[i] = edges[i].map(x => -x)
+  edges[i] = [-edges[i][0], -edges[i][1], -edges[i][2]]
 
   const token = getToken(edges)
   for (let i = order; i >= 0; i--) {
@@ -295,25 +334,56 @@ const getTriangles = (edges, order) => {
 
   const vertices = []
   if (order > 0) {
-    if (!reflectOn(edges, 0, order)) {
+    if (!reflectOn(edges, 2, order)) {
       return
     }
   }
+  /*
 
-  vertices.push(invertTriangle(edges)[0])
+  |q\
+  |  \   2
+  |   \
+0 |   r\
+  |   /
+  |p/   1
+  
+  C
+  */
+
+  const center =
+    order % 2 === 1
+      ? intersect(edges[1], edges[0])
+      : intersect(edges[0], edges[1])
+  if (order % 2 === 1) {
+    vertices.push(intersect(edges[2], edges[0]))
+    vertices.push(intersect(edges[1], edges[2]))
+  } else {
+    vertices.push(intersect(edges[0], edges[2]))
+    vertices.push(intersect(edges[2], edges[1]))
+  }
   triangles[order].push([...edges])
 
-  const center = invertTriangle(edges)[1]
   for (let n = 0; n < settings.p * 2 - 1; n++) {
-    if (reflectOn(edges, 1 + (n % 2), order)) {
-      vertices.push(invertTriangle(edges)[((n + 1) % 2) * 2])
+    if (reflectOn(edges, (n + 1) % 2, order)) {
+      if (order % 2 === 1) {
+        vertices.push(intersect(edges[n % 2], edges[2]))
+      } else {
+        vertices.push(intersect(edges[2], edges[n % 2]))
+      }
       triangles[order].push([...edges])
     }
   }
 
   for (let t = 0; t < transformations.length; t++) {
     const { type, parameter } = transformations[t]
-    const trans = type === 'scale' ? hyperbolicScale : hyperbolicTranslate
+    const trans =
+      model === 'hyperbolic'
+        ? type === 'scale'
+          ? hyperbolicScale
+          : hyperbolicTranslate
+        : type === 'scale'
+        ? ellipticScale
+        : ellipticTranslate
     trans(center, parameter)
     for (let v = 0; v < vertices.length; v++) {
       trans(vertices[v], parameter)
@@ -328,25 +398,31 @@ const getTriangles = (edges, order) => {
   polygons[order].push(polygon)
 }
 
+const ellipticTranslate = (vertex, offset) => {
+  let [xe, ye, ze] = vertex
+  const [xt, yt, zt] = offset
+  const cxt = Math.cos(Math.asin(xt))
+  const cyt = Math.cos(Math.asin(yt))
+  vertex[0] = xe
+  vertex[1] = ye * cyt - ze * yt
+  vertex[2] = ye * yt + ze * cyt
+  ;[xe, ye, ze] = vertex
+  vertex[0] = xe * cxt + ze * xt
+  vertex[2] = -xe * xt + ze * cxt
+}
+
 const hyperbolicTranslate = (vertex, offset) => {
-  const [xe, ye, ze] = vertex
+  let [xe, ye, ze] = vertex
   const [xt, yt, zt] = offset
 
-  const tt2 = xt * xt + yt * yt
-  if (tt2 < 1e-9) {
-    return
-  }
-  const xb = xt * (zt + 1)
-  const yb = yt * (zt + 1)
-  const zb = -tt2
-  const pb = xe * xb + ye * yb - ze * zb
-  const bb = xb * xb + yb * yb - zb * zb
-  const k = (2 * pb) / bb
-  const [x, y, z] = [xe - k * xb, ye - k * yb, ze - k * zb]
-  const l = (2 * (xt * x + yt * y)) / tt2
-  vertex[0] = x - l * xt
-  vertex[1] = y - l * yt
-  vertex[2] = z
+  const cxt = Math.cosh(Math.asinh(xt))
+  const cyt = Math.cosh(Math.asinh(yt))
+  vertex[0] = xe
+  vertex[1] = ye * cyt + ze * yt
+  vertex[2] = ye * yt + ze * cyt
+  ;[xe, ye, ze] = vertex
+  vertex[0] = xe * cxt - ze * xt
+  vertex[2] = -xe * xt + ze * cxt
 }
 
 const hyperbolicRotate = (vertex, theta) => {
@@ -379,27 +455,18 @@ const hyperbolicGyration = (p1, p2, p3) => {
   return [x * xc - y * yc, y * xc + yc * x]
 }
 
-const invertTriangle = points => {
-  const inverted = []
-  for (let i = 0; i < points.length; i++) {
-    inverted.push(intersect(points[i], points[(i + 1) % points.length]))
-  }
-  return inverted
-}
-
 const translate = offset => {
-  if (backend === '2d') {
-    offset[0] *= -1
-  }
   const translation = [...offset, 1]
   transformations.push({ type: 'translate', parameter: translation })
+  const translator =
+    model === 'hyperbolic' ? hyperbolicTranslate : ellipticTranslate
   for (let o = 0; o < polygons.length; o++) {
     const orderPolygons = polygons[o]
     for (let i = 0; i < orderPolygons.length; i++) {
       const { vertices, center } = orderPolygons[i]
-      hyperbolicTranslate(center, translation)
+      translator(center, translation)
       for (let j = 0; j < vertices.length; j++) {
-        hyperbolicTranslate(vertices[j], translation)
+        translator(vertices[j], translation)
       }
     }
   }
@@ -434,13 +501,13 @@ const scale = scale => {
 
 const line = (u, v) => {
   const curve = !settings.straight && settings.projection !== 'klein'
-  const pu = toDisk(fromHyperboloid(u))
-  const pv = toDisk(fromHyperboloid(v))
+  const pu = toDisk(project(u))
+  const pv = toDisk(project(v))
   const realDist =
     curve && Math.sqrt((pu[0] - pv[0]) ** 2 + (pu[1] - pv[1]) ** 2)
   if (curve && realDist > 20 - settings.curvePrecision) {
-    const ab = u[0] * v[0] + u[1] * v[1] - u[2] * v[2]
-    const t2s = t => ab * t + Math.sqrt(t * t * (ab * ab - 1) + 1)
+    const ab = dot(u, v)
+    const t2s = t => Math.sqrt(t * t * (ab * ab - 1) + 1) - sign() * ab * t
     let curveStep = Math.max(0.01, (20 - settings.curvePrecision) / realDist)
     for (let t = 1 - curveStep; t > 0; t -= curveStep) {
       const s = t2s(t)
@@ -450,9 +517,9 @@ const line = (u, v) => {
         u[1] * t + v[1] * s,
         u[2] * t + v[2] * s,
       ]
-      const p = toDisk(fromHyperboloid(T))
+      const p = toDisk(project(T))
       if (
-        settings.projection === 'ortho' &&
+        settings.projection === 'orthographic' &&
         (p[0].width || p[0] < 0 || p[1].height || p[1] < 0)
       ) {
         break
@@ -464,7 +531,7 @@ const line = (u, v) => {
 }
 
 const renderPolygon = ({ vertices, center, order }) => {
-  if (vertices.length < 3) {
+  if (vertices.length < 2) {
     return
   }
   if (backend === '3d') {
@@ -495,10 +562,10 @@ const renderPolygon = ({ vertices, center, order }) => {
   }
 
   let polyVertices = []
-  if (settings.r === 2) {
+  if (settings.r === 2 && model === 'hyperbolic') {
     // Optimization
     for (let i = 0; i < settings.p; i++) {
-      polyVertices.push(vertices[(i * 2 + 1) % vertices.length])
+      polyVertices.push(vertices[(i * 2) % vertices.length])
     }
   } else {
     polyVertices = vertices
@@ -522,7 +589,7 @@ const renderPolygon = ({ vertices, center, order }) => {
 const renderVertices = vertices => {
   ctx.beginPath()
 
-  const p0 = toDisk(fromHyperboloid(vertices[0]))
+  const p0 = toDisk(project(vertices[0]))
   ctx.moveTo(p0[0], p0[1])
 
   for (let j = 0, m = vertices.length; j < m; j++) {
@@ -694,6 +761,10 @@ const render = () => {
     geometry.setDrawRange(0, pos)
     renderer.render(scene, camera)
   }
+  // ctx.save()
+  // ctx.fillStyle = 'white'
+  // renderVertices(invertTriangle(getRootTriangle()))
+  // ctx.restore()
   showStats.showStats && stats.end()
 }
 
@@ -705,33 +776,16 @@ const gui = new GUI({
 gui.remember(settings)
 
 const pqrCheckRegenerate = () => {
+  model = getModel(settings)
   if (reverting) {
     return
   }
-  const a = [~~settings.p, ~~settings.q, ~~settings.r]
-  const check = () => 1 / a[0] + 1 / a[1] + 1 / a[2] < 1
-  while (!check()) {
-    let i0 = ~~(Math.random() * 3)
-    for (let i = 0; i < 3; i++) {
-      for (let z = 0; z < 3; z++) {
-        a[(i0 + i) % 3]++
-        if (check()) {
-          break
-        }
-        if (i < 2) {
-          a[(i0 + i) % 3]--
-        }
-      }
-    }
-  }
-  settings.p !== a[0] && (settings.p = a[0])
-  settings.q !== a[1] && (settings.q = a[1])
-  settings.r !== a[2] && (settings.r = a[2])
 
   if (settings.p !== _pqr.p || settings.q !== _pqr.q || settings.r !== _pqr.r) {
     regenerate()
   }
 }
+
 const debounce = (func, wait, immediate) => {
   let timeout
   return function (...args) {
@@ -870,6 +924,7 @@ window.hyperball = {
   translate,
   transformations,
   render,
+  geometry,
 }
 
 renderer.domElement.id = 'c3d'
